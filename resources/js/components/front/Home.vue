33e0e8655c5ec38027400b468198eeb35aefd208
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-10-05 21:13:42 +0100

    any sin

--- resources/js/components/front/Home.vue
+++ resources/js/components/front/Home.vue
@@ -299,7 +299,7 @@ export default{
         get: '',
         parameters: [],
         errors: [],
-        results: 0,
+        results: null,
         response: '',
       },
     }
@@ -309,13 +309,27 @@ mounted: function() {
    this.displayBox1();
 },
 methods: {
+    formatDate(inputDate) {
+      const date = new Date(inputDate);
+      const day = date.getDate();
+      const month = date.getMonth() + 1; // Months are zero-based, so add 1
+      const year = date.getFullYear();
+
+      // Ensure that day and month have leading zeros if necessary
+      const formattedDay = day < 10 ? `0${day}` : day;
+      const formattedMonth = month < 10 ? `0${month}` : month;
+
+      // Combine the formatted parts into the final date string
+      return `${formattedDay}/${formattedMonth}/${year}`;
+    },
+
     getGames() {
       var currentDate = new Date();
       var formattedDate = currentDate.toISOString().split('T')[0]; // Formats the date as 'yyyy-mm-dd'
 
       var myHeaders = new Headers();
       myHeaders.append("x-rapidapi-key", "71d95ba09f4094fa6f5f291f089cb27b");
-      myHeaders.append("x-rapidapi-host", "v1.basketball.api-sports.io/teams?country_id=106");
+      myHeaders.append("x-rapidapi-host", "v1.basketball.api-sports.io/leagues?country_id=106&date=" + formattedDate);
 
       var requestOptions = {
         method: 'GET',
@@ -323,7 +337,7 @@ methods: {
         redirect: 'follow'
       };
 
-      fetch("https://v1.basketball.api-sports.io/teams?country_id=106", requestOptions)
+      fetch("https://v1.basketball.api-sports.io/leagues?country_id=106&date=" + formattedDate, requestOptions)
         .then(response => response.json())
         .then(result => {
           console.log(result)
@@ -377,6 +391,7 @@ methods: {
     let res = new Intl.NumberFormat('fr-FR', { maximumSignificantDigits: 3 }).format(num);
     return res;
 },
+
     getImgUrl(pic) {
     return  pic;
 },
